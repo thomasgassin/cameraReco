f13ff32129d9db2a786d58897257e2f236e34f30
thomasgassin <thomas.gassin@gmail.com>
2022-11-15 00:00:00 +0100

    pb authentification ssh

--- codecamera.py
+++ codecamera.py
@@ -4,7 +4,7 @@ from time import sleep
 camera = PiCamera()
 #camera.resolution = (300, 200)
 camera.preview_fullscreen=False 
-camera.preview_window=(620, 320, 640, 520) 
+camera.preview_window=(620, 320, 640, 480) 
 camera.start_preview()
-sleep(5)
+sleep(3)
 camera.stop_preview()
